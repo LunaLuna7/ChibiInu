094f468435ba8547851ef6af2882591a6670ef98
Luis <lfluna@uci.edu>
2018-07-01 23:32:07 -0700

    Camera + char movement

--- ChibiInuGame/Assets/scripts/WalkingPhase/CameraMovement.cs
+++ ChibiInuGame/Assets/scripts/WalkingPhase/CameraMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-    private GameObject player;
+    public GameObject target;
     private Rigidbody2D rb;
     private bool isGrounded;
 
@@ -17,34 +17,34 @@ public class CameraMovement : MonoBehaviour {
     public float smoothDown;
     public float currentHeight;
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
-        currentHeight = player.transform.position.y;
-        rb = player.GetComponent<Rigidbody2D>();
+        //player = GameObject.FindGameObjectWithTag("Player");
+        currentHeight = target.transform.position.y;
+        rb = target.GetComponent<Rigidbody2D>();
         
     }
 	
 	
 	void LateUpdate () {
-        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
-        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        float x = Mathf.Clamp(target.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(target.transform.position.y, yMin, yMax);
 
        
-        if (player.transform.position.y > currentHeight + 5) //above
+        if (target.transform.position.y > currentHeight + 5) //above
         {
             //try to do y + some formula that the higher the current height the more you can see below you
             transform.position = Vector3.Lerp(transform.position, new Vector3(x, y + 1 , gameObject.transform.position.z), smoothUp); //y + 1f
         }
-        else if(player.transform.position.y < currentHeight)
+        else if(target.transform.position.y < currentHeight)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(x, y + 1f, gameObject.transform.position.z), smoothDown);
         }
-        else if(player.transform.position.y < currentHeight + 5)
+        else if(target.transform.position.y < currentHeight + 5)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(x, currentHeight, gameObject.transform.position.z), smoothDown);   
         }
         if (rb.velocity.y == 0)
         {
-            currentHeight = player.transform.position.y;
+            currentHeight = target.transform.position.y;
         }
        
         Debug.Log(rb.velocity.y);
