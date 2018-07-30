ba484feee799e38626cedd8ba346d37fc04d017a
Luis <lfluna@uci.edu>
2018-07-29 23:53:59 -0700

    tilemap and plosih level 1

---
+++ ChibiInuGame/Assets/scripts/LevelSelectManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectManager : MonoBehaviour {
+
+    [System.SerializableAttribute]
+    public class Level
+    {
+        public string name;
+        public bool unlocked;
+        public Transform transform;
+    }
+
+    public List<Level> levels;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ChibiInuGame/Assets/scripts/Player/LvlSelectMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlSelectMovement : MonoBehaviour {
+
+    public LevelSelectManager levelSelectManager;
+    public int position;
+    private const float speed = 10f;
+
+	void Start () {
+        position = 0; //current level
+        transform.position = levelSelectManager.levels[position].transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MovePlayer(position);
+        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position == levelSelectManager.levels[position].transform.position)
+        {
+            if(position + 1 < levelSelectManager.levels.Capacity)
+                position += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position == levelSelectManager.levels[position].transform.position)
+        {
+            if (position - 1 >= 0)
+                position -= 1;
+        }
+
+
+
+
+    }
+
+    void MovePlayer(int i)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, levelSelectManager.levels[i].transform.position, speed * Time.deltaTime);
+    }
+
+    
+}
