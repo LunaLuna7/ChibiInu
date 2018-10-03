2acf339eb1595131e4468d894c23d99649a0c2de
Luis <lfluna@uci.edu>
2018-10-02 19:54:16 -0700

    tile fix & camera weird stuff

---
+++ ChibiInuGame/Assets/scripts/Player/PlayerCameraSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraSwitch : MonoBehaviour {
+
+    private SwitchCamera sw;
+    public CharacterController2D characterController2D;
+    public int cameraRight;
+    public int cameraLeft;
+
+    // Use this for initialization
+    void Start () {
+        sw = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<SwitchCamera>();
+
+        characterController2D = GetComponent<CharacterController2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+         if (characterController2D.m_FacingRight)
+         {
+            sw.ChangeCamera(cameraRight);
+         }
+            else if (!characterController2D.m_FacingRight)
+         {
+            sw.ChangeCamera(cameraLeft);
+         }
+	}
+}
--- ChibiInuGame/Assets/scripts/SwitchCamera.cs
+++ ChibiInuGame/Assets/scripts/SwitchCamera.cs
@@ -2,23 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SwitchCamera : MonoBehaviour {
 
-    //public GameObject cam1;
-    //public GameObject cam2;
+    
     public GameObject[] cameras;
-
+    
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-        
+       
 
 	}
+
 
     public void ChangeCamera(int activeCam)
     {
@@ -31,8 +30,7 @@ public class SwitchCamera : MonoBehaviour {
             cameras[i].SetActive(false);
         }
         cameras[activeCam - 1].SetActive(true);
-        
-       
+          
     }
     
 }
