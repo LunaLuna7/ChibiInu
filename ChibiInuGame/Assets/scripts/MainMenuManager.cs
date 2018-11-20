6f592f169e4b74f48418639f31783303eaf54d7b
Weiyan Zhu <weiyanz@uci.edu>
2018-11-20 00:38:41 -0800

    Use Keyboard to control main menu- main page

--- ChibiInuGame/Assets/scripts/MainMenuManager.cs
+++ ChibiInuGame/Assets/scripts/MainMenuManager.cs
@@ -6,21 +6,68 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour {
 	public GameObject chooseFilePage;
 	public Text[] saveSlotUI = new Text[3];
+	public GameObject arrow;
+	public GameObject[] buttons;
 	private SaveData[] saveDatas = new SaveData[3];
 	// Use this for initialization
 	void Start () {
 		chooseFilePage.SetActive(false);
+		//UpdateArrow(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		CheckMainInput();
+	}
+
+	//========================================================
+	//deal with inputs
+	//========================================================
+	private int mainArrowIndex = 0;
+	
+	public void CheckMainInput()
+	{
+		//when press up button
+		if(Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0 && mainArrowIndex >0)
+		{
+			--mainArrowIndex;
+			UpdateArrow(mainArrowIndex);
+		}
+		//when press down
+		else if(Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0 && mainArrowIndex < buttons.Length - 1)
+		{
+			++mainArrowIndex;
+			UpdateArrow(mainArrowIndex);
+		}
+		//when press Space
+		else if(Input.GetButtonDown("Jump"))
+		{
+			switch(mainArrowIndex)
+			{
+				//start game
+				case 0:
+					StartGameButton();
+					break;
+				case 1:
+					break;
+				//quit game
+				case 2:
+					QuitButton();
+					break;
+			}
+		}
+	}
+
+	private void UpdateArrow(int index)
+	{
+		arrow.GetComponent<RectTransform>().position = new Vector3(arrow.GetComponent<RectTransform>().position.x, 
+															  buttons[index].GetComponent<RectTransform>().position.y, 
+															  arrow.GetComponent<RectTransform>().position.z);
 	}
 
 	//========================================================
 	//Buttons
 	//========================================================
-	
 	//let player choose from one of the three saveData slot
 	public void StartGameButton()
 	{
@@ -66,6 +113,7 @@ public class MainMenuManager : MonoBehaviour {
 	public void QuitButton()
 	{
 		Application.Quit();
+		Debug.Log("Quit Game");
 	}
 	//========================================================
 	//Modifing UI contents
