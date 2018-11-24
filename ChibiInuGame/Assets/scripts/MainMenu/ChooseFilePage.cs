29287bb3ac5c493c3ea767f030bcbd186604f374
Weiyan Zhu <weiyanz@uci.edu>
2018-11-24 00:23:40 -0800

    Navigate MainMenu and SaveFilePage with keyboard

--- ChibiInuGame/Assets/scripts/MainMenu/ChooseFilePage.cs
+++ ChibiInuGame/Assets/scripts/MainMenu/ChooseFilePage.cs
@@ -1,16 +1,102 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChooseFilePage : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+	public GameObject arrow;
+	public GameObject[] buttons;
+	public Text[] saveSlotUI = new Text[3];
+	private SaveData[] saveDatas = new SaveData[3];
 	void Update () {
-		
+		CheckInput();
+	}
+
+	//========================================================
+	//Choosing file/ Change index
+	//========================================================
+	private int arrowIndex = 0;
+	public void CheckInput()
+	{
+		//when press up button
+		if(Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0 && arrowIndex >0)
+		{
+			UpdateArrow(--arrowIndex);
+		}
+		//when press down
+		else if(Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0 && arrowIndex < buttons.Length - 1)
+		{
+			UpdateArrow(++arrowIndex);
+		}
+		//when press Space
+		else if(Input.GetButtonDown("Jump"))
+			LoadSaveFile(arrowIndex);
+		else if(Input.GetButtonDown("Cancel") || Input.GetMouseButtonDown(2))
+			ReturnFromChooseFile();
+	}
+
+	private void UpdateArrow(int index)
+	{
+		arrowIndex = index;
+		arrow.GetComponent<RectTransform>().position = new Vector3(arrow.GetComponent<RectTransform>().position.x, 
+															  buttons[index].GetComponent<RectTransform>().position.y, 
+															  arrow.GetComponent<RectTransform>().position.z);
+	}
+	public void ReturnFromChooseFile()
+	{
+		Debug.Log(1);
+		gameObject.SetActive(false);
+	}
+
+	//========================================================
+	//Save Contents
+	//========================================================
+	public void InitilizeSavePage()
+	{
+		//load files
+		saveDatas[0] = SaveManager.Load("save1");
+		saveDatas[1] = SaveManager.Load("save2");
+		saveDatas[2] = SaveManager.Load("save3");
+		//update the contents
+		for(int index = 0; index < saveDatas.Length; ++index)
+		{
+			UpdateSaveSlotUI(saveSlotUI[index], saveDatas[index]);
+		}
+		UpdateArrow(0);
+	}
+
+	public void LoadSaveFile(int index)
+	{
+		//no safe data, ask users enter name and create a new save file for them
+		if(saveDatas[index] == null)
+		{
+			//get name
+			//create new saveData
+			SaveData newData = new SaveData("Test");
+			//SaveManager.DebugJsonData(newData);
+			SaveManager.dataInUse = newData;
+			SaveManager.filename = "save" + (index + 1);
+			SaveManager.Save(SaveManager.filename);
+		}else//load the save file and update the levels
+		{
+			SaveManager.dataInUse = saveDatas[index];
+			SaveManager.filename = "save" + (index + 1);
+		}
+		//go to levelSelectionScene
+		UnityEngine.SceneManagement.SceneManager.LoadScene("LevelSelect");
+	}
+
+	
+	//========================================================
+	//Modifing UI contents
+	//========================================================
+	public void UpdateSaveSlotUI(Text slotUI, SaveData data)
+	{
+		if(data == null){
+			slotUI.text = "Start a New Game";
+		}
+		else{
+			slotUI.text = data.playerName;// + "\n" + (data.highestLevelAchieved/10) + "%";
+		}
 	}
 }
--- ChibiInuGame/Assets/scripts/MainMenu/MainMenuManager.cs
+++ ChibiInuGame/Assets/scripts/MainMenu/MainMenuManager.cs
@@ -5,10 +5,9 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
 	public GameObject chooseFilePage;
-	public Text[] saveSlotUI = new Text[3];
 	public GameObject arrow;
 	public GameObject[] buttons;
-	private SaveData[] saveDatas = new SaveData[3];
+	
 	// Use this for initialization
 	void Start () {
 		chooseFilePage.SetActive(false);
@@ -17,7 +16,8 @@ public class MainMenuManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		CheckMainInput();
+		if(!chooseFilePage.activeSelf)
+			CheckMainInput();
 	}
 
 	//========================================================
@@ -72,60 +72,16 @@ public class MainMenuManager : MonoBehaviour {
 	public void StartGameButton()
 	{
 		chooseFilePage.SetActive(true);
-		//load files
-		saveDatas[0] = SaveManager.Load("save1");
-		saveDatas[1] = SaveManager.Load("save2");
-		saveDatas[2] = SaveManager.Load("save3");
-		//update the contents
-		for(int index = 0; index < saveDatas.Length; ++index)
-		{
-			UpdateSaveSlotUI(saveSlotUI[index], saveDatas[index]);
-		}
+		chooseFilePage.GetComponent<ChooseFilePage>().InitilizeSavePage();
 	}
 
 	//load the level info and go to the level switch page
-	public void LoadSaveFile(int index)
-	{
-		//no safe data, ask users enter name and create a new save file for them
-		if(saveDatas[index] == null)
-		{
-			//get name
-			//create new saveData
-			SaveData newData = new SaveData("Test");
-			//SaveManager.DebugJsonData(newData);
-			SaveManager.dataInUse = newData;
-			SaveManager.filename = "save" + (index + 1);
-			SaveManager.Save(SaveManager.filename);
-		}else//load the save file and update the levels
-		{
-			SaveManager.dataInUse = saveDatas[index];
-			SaveManager.filename = "save" + (index + 1);
-		}
-		//go to levelSelectionScene
-		UnityEngine.SceneManagement.SceneManager.LoadScene("LevelSelect");
-	}
-
-	public void ReturnFromChooseFileButton()
-	{
-		chooseFilePage.SetActive(false);
-	}
 
 	public void QuitButton()
 	{
 		Application.Quit();
 		Debug.Log("Quit Game");
 	}
-	//========================================================
-	//Modifing UI contents
-	//========================================================
-	public void UpdateSaveSlotUI(Text slotUI, SaveData data)
-	{
-		if(data == null){
-			slotUI.text = "Start a New Game";
-		}
-		else{
-			slotUI.text = data.playerName;// + "\n" + (data.highestLevelAchieved/10) + "%";
-		}
-	}
+
 
 }
