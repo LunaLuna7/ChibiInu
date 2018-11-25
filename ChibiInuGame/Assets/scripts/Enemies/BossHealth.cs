4e2908f02961ad547869a3b28cbfd84d337451ed
Luis Luna <lfluna@uci.edu>
2018-11-24 19:58:37 -0800

    Boss 2

---
+++ ChibiInuGame/Assets/scripts/Enemies/BossHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour {
+
+    [HideInInspector] public StateController stateController;
+    public float health;
+    private float maxHealth;
+    public Image healthBar;
+
+    private void Start()
+    {
+        stateController = GetComponent<StateController>();
+        health = maxHealth = stateController.enemyStats.HP;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        float currentHp = health / maxHealth;
+        healthBar.fillAmount = currentHp;
+        if (health <= 0)
+        {
+
+            gameObject.SetActive(false);
+            health = stateController.enemyStats.HP;
+        }
+    }
+}
