6bc55af85d9e8a6346d06d7b2555762f31538af0
Luis Luna <lfluna@uci.edu>
2018-12-18 15:36:04 -0800

    Polish lv2 added boss state machine

---
+++ ChibiInuGame/Assets/scripts/Boss2/States/IntroState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroState : IState
+{
+    public void EnterState()
+    {
+
+        Debug.Log("Battle Starts!");
+    }
+
+    public void ExecuteState()
+    {
+        
+    }
+
+    public void ExitState()
+    {
+        
+    }
+}
