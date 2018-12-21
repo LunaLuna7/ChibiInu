5ab6fdf94aa127ae9f85e6cca337d104fb795335
Luis Luna <lfluna@uci.edu>
2018-12-20 17:59:46 -0800

    Ch3 level 1 skeleton
    -knight mechanic
    -laser mechanic and asset

---
+++ ChibiInuGame/Assets/scripts/ScriptableObjects/ChaseAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Chase")]
+public class ChaseAction : Action {
+
+    public override void Act(StateController controller)
+    {
+        Chase(controller);
+    }
+
+    void Chase(StateController controller)
+    {
+        controller.rb.MovePosition(Vector3.Lerp(controller.transform.position, controller.player.transform.position, controller.enemyStats.moveSpeed));
+        controller.transform.position = Vector2.MoveTowards(controller.transform.position, controller.player.transform.position, controller.enemyStats.moveSpeed * Time.deltaTime);
+    }
+}
