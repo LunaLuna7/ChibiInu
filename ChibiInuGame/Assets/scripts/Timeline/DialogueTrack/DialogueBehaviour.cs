d8f918d0b5b4ea75faeed0c3b650004986dd1832
Weiyan Zhu <weiyanz@uci.edu>
2019-01-03 13:04:03 -0800

    Fix level1-3 bossintro cutscene

--- ChibiInuGame/Assets/scripts/Timeline/DialogueTrack/DialogueBehaviour.cs
+++ ChibiInuGame/Assets/scripts/Timeline/DialogueTrack/DialogueBehaviour.cs
@@ -7,7 +7,7 @@ using UnityEngine.Playables;
 public class DialogueBehaviour : PlayableBehaviour {
     [SerializeField] private int startDialogueIndex;
     [SerializeField] private int endDialogueIndex;
-    [SerializeField] private bool hasPlayed = false;
+    private bool hasPlayed = false;
 
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
