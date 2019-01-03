30926f125d0ad4849e402f0c5f2291e743bb10c8
Weiyan Zhu <weiyanz@uci.edu>
2019-01-02 23:15:13 -0800

    custom dialogue track structure

---
+++ ChibiInuGame/Assets/scripts/Timeline/DialogueTrack/DialogueBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class DialogueBehaviour : PlayableBehaviour {
+    [SerializeField] private int startDialogueIndex;
+    [SerializeField] private int endDialogueIndex;
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        var dialogueManager = playerData as DialogueManager;
+        
+    }
+
+}
