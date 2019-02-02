7968ae531bbe2a93656124ccbd7ad5084e961c87
Weiyan Zhu <weiyanz@uci.edu>
2019-02-01 20:48:20 -0800

    create files

--- ChibiInuGame/Assets/scripts/Boss2/States/FastSpikeState.cs
+++ ChibiInuGame/Assets/scripts/Boss2/States/FastSpikeState.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FluteSpikeSong : IState
-{
+public class FastSpikeState : IState{
+
+	BossWorld2 controller;
+
+    public FastSpikeState(BossWorld2 c)
+    {
+        controller = c;
+    }
 
     public void EnterState()
     {
-        throw new System.NotImplementedException();
+        controller.inState = true;
     }
 
     public void ExecuteState()
     {
-        throw new System.NotImplementedException();
+
     }
 
     public void ExitState()
     {
-        throw new System.NotImplementedException();
+        controller.inState = false;
     }
 }
---
+++ ChibiInuGame/Assets/scripts/Boss2/States/WindFurryState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindFurryState : IState {
+
+	BossWorld2 controller;
+
+    public WindFurryState(BossWorld2 c)
+    {
+        controller = c;
+    }
+
+    public void EnterState()
+    {
+        controller.inState = true;
+    }
+
+    public void ExecuteState()
+    {
+
+    }
+
+    public void ExitState()
+    {
+        controller.inState = false;
+    }
+}
