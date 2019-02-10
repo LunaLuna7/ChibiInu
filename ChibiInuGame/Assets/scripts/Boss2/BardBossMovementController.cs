32102c34172e79f563ffd2cc70bc869ea4345cd1
Weiyan Zhu <weiyanz@uci.edu>
2019-02-09 21:11:03 -0800

    set boundary in code

--- ChibiInuGame/Assets/scripts/Boss2/BardBossMovementController.cs
+++ ChibiInuGame/Assets/scripts/Boss2/BardBossMovementController.cs
@@ -8,16 +8,26 @@ public class BardBossMovementController : MonoBehaviour {
 	public float speed;
 	private SpriteRenderer spriteRenderer;
 	private Rigidbody2D rigid;
+	[Header("Boundary Detection")]
+	public Transform leftLowerPoint;
+	public Transform rightUpperPoint;
+	private float centerX;
+	private float centerY;
+	public float halfWidth;
+	public float halfHeight;
 
 	void Awake()
 	{
 		rigid = GetComponent<Rigidbody2D>();
 		spriteRenderer = GetComponent<SpriteRenderer>();
+		centerX = (leftLowerPoint.position.x + rightUpperPoint.position.x)/2;
+		centerY = (leftLowerPoint.position.y + rightUpperPoint.position.y)/2;
 	}
+
 
 	void Update()
 	{
-		Debug.Log(rigid.velocity);
+		CheckBoundary();
 	}
 
 	//move towards a random direction
@@ -26,15 +36,14 @@ public class BardBossMovementController : MonoBehaviour {
 		if(speed > 0)
 			this.speed = speed;
 		float radian = Random.Range(0, 6.28f);
-		SetDirection(radian);
-		rigid.velocity = this.speed * direction;
-		ChangeFaceDirection();
+		SetVelocityRadian(radian);
 	}
 
-	private void SetDirection(float radian)
+	public void ContinueMoving()
 	{
-		direction = new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+		rigid.velocity = this.speed * direction;
 	}
+
 	public void StopMoving()
 	{
 		ChangeSpeed(0);
@@ -47,7 +56,84 @@ public class BardBossMovementController : MonoBehaviour {
 		rigid.velocity = this.speed * direction;
 	}
 
-	//change velocity each time hitting the boundary
+	private void CheckBoundary()
+	{
+		Vector3 pos = transform.position;
+		float radian = 0;
+		//x position out of range
+		//touch left
+		if(pos.x - halfWidth < leftLowerPoint.position.x)
+		{
+			//set position in bound
+			transform.position = new Vector3(leftLowerPoint.position.x + halfWidth ,pos.y, pos.z);
+			//change direction depends on y
+			if(pos.y > centerY)
+			{
+				radian = Random.Range(5f, 6.28f); // go to right down
+			}else{
+				radian = Random.Range(0f, 1.4f); // go to right up
+			}
+			SetVelocityRadian(radian);
+		}//right
+		else if (pos.x + halfWidth> rightUpperPoint.position.x)
+		{
+			//set position in bound
+			transform.position = new Vector3(rightUpperPoint.position.x - halfWidth ,pos.y, pos.z);
+			//change direction depends on y
+			if(pos.y > centerY)
+			{
+				radian = Random.Range(3.14f, 4.5f); // go to left down
+			}else{
+				radian = Random.Range(1.57f, 3f); // go to left up
+			}
+			SetVelocityRadian(radian);
+		}
+		//y position out of range
+		//touch bottom
+		if(pos.y - halfHeight < leftLowerPoint.position.y)
+		{
+			//set position in bound
+			transform.position = new Vector3(pos.x, leftLowerPoint.position.y + halfWidth, pos.z);
+			//change direction depends on y
+			if(pos.x > centerX)
+			{
+				radian = Random.Range(1.57f, 3.14f); // go to left up
+			}else{
+				radian = Random.Range(0f, 1.57f); // go to right up
+			}
+			SetVelocityRadian(radian);
+		}//top
+		else if (pos.y + halfHeight > rightUpperPoint.position.y)
+		{
+			//set position in bound
+			transform.position = new Vector3(pos.x, rightUpperPoint.position.y - halfWidth, pos.z);
+			//change direction depends on y
+			if(pos.x > centerX)
+			{
+				radian = Random.Range(3.14f, 4.71f); // go to left down
+			}else{
+				radian = Random.Range(4.71f, 6.28f); // go to right down
+			}
+			SetVelocityRadian(radian);
+		}
+	}
+
+	//change face direction depends on the move direction
+	private void ChangeFaceDirection()
+	{
+		if(direction.x >= 0)
+			spriteRenderer.flipX = false;
+		else
+			spriteRenderer.flipX = true;
+	}
+
+	private void SetVelocityRadian(float radian)
+	{
+		direction = new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+		rigid.velocity = direction * speed;
+		ChangeFaceDirection();
+	}
+	/* change velocity each time hitting the boundary
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if(other.collider.tag == "Wall")
@@ -68,15 +154,8 @@ public class BardBossMovementController : MonoBehaviour {
 		SetDirection(radian);
 		rigid.velocity = direction * speed;
 		ChangeFaceDirection();
-	}
+	}*/
 
-	//change face direction depends on the move direction
-	private void ChangeFaceDirection()
-	{
-		if(direction.x >= 0)
-			spriteRenderer.flipX = false;
-		else
-			spriteRenderer.flipX = true;
-	}
+
 
 }
