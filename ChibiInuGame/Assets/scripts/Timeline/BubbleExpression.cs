8056ed1f624bdb5f22b3fd83173d37a217e78275
Weiyan Zhu <weiyanz@uci.edu>
2019-02-15 23:52:12 -0800

    bubble expression

---
+++ ChibiInuGame/Assets/scripts/Timeline/BubbleExpression.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleExpression : MonoBehaviour {
+	public GameObject target;
+	public Vector2 posDiffer;
+	// Use this for initialization
+	void OnEnable () {
+		transform.position = target.transform.position + new Vector3(posDiffer.x, posDiffer.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position + new Vector3(posDiffer.x, posDiffer.y, 0);
+	}
+}
