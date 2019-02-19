6065bda80a20fe86915e80e26131a74c50064dab
Luis Luna <lfluna@uci.edu>
2019-02-19 12:23:04 -0800

    ch3lv2

---
+++ ChibiInuGame/Assets/scripts/PowerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerManager : MonoBehaviour {
+
+    public CastlePlatform castlePlatform;
+    public List<GameObject> objectsToAppear;
+    public List<GameObject> objectsToDisappear;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnEnemies()
+    {
+        foreach(GameObject enemies in objectsToAppear)
+        {
+            enemies.SetActive(true);
+        }
+        foreach (GameObject each in objectsToDisappear)
+            each.SetActive(false);
+    }
+}
