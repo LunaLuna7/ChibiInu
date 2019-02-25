c99e459f186ea9ef81fe421790831d41fb9c6c16
Luis Luna <lfluna@uci.edu>
2019-02-24 23:54:48 -0800

    State Machine AI chase update
    Doesnt see player on shield

---
+++ ChibiInuGame/Assets/scripts/ScriptableObjects/InSightDecision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/InSight")]
+public class InSightDecision : Decision {
+
+    public override bool Decide(StateController controller)
+    {
+        bool playerVisible;
+        playerVisible = !controller.player.GetComponent<CharacterController2D>().m_OnShield;
+
+        return playerVisible;
+    }
+
+}
