8e2a2979ed4ea7726b46947c29f3faafe17b8de7
Weiyan Zhu <weiyanz@uci.edu>
2019-03-03 21:23:07 -0800

    Knight Boss Movement

--- ChibiInuGame/Assets/scripts/Boss3/KnightBossMovementController.cs
+++ ChibiInuGame/Assets/scripts/Boss3/KnightBossMovementController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KnightBossMovementController : MonoBehaviour {
-
+	public Transform[] possibleLocations;
+	private Rigidbody2D rigid;
+	private SpriteRenderer sprite;
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = new Vector3(possibleLocations[0].position.x, possibleLocations[0].position.y, transform.position.z);
+		rigid = GetComponent<Rigidbody2D>();
+		sprite = GetComponent<SpriteRenderer>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public IEnumerator MoveTo(Vector3 position, float speed)
+	{
+		float distance = Vector3.Distance(transform.position, position);
+		float totalTime = distance/speed;
+		Vector3 diff = position - transform.position;
+		Vector3 originPos = transform.position;
+		//change sprite
+		if(diff.x > 0)
+		{
+			sprite.flipX = false;
+		}else if(diff.x < 0){
+			sprite.flipX = true;
+		}
+		//move
+		for(float time = 0; time< totalTime; time += Time.deltaTime)
+		{
+			Vector3 tempPos = originPos + diff * time/totalTime;
+			tempPos.z = originPos.z;
+			rigid.MovePosition(tempPos);
+			yield return new WaitForEndOfFrame();
+		}
+		rigid.MovePosition(new Vector3(position.x, position.y, originPos.z));
 	}
 }
--- ChibiInuGame/Assets/scripts/Boss3/States/KnightBossMovingState.cs
+++ ChibiInuGame/Assets/scripts/Boss3/States/KnightBossMovingState.cs
@@ -13,7 +13,7 @@ public class KnightBossMovingState : IState {
 
 	public void EnterState()
 	{
-
+		controller.StartCoroutine(Move(12));
 	}
 
 	public void ExecuteState()
@@ -23,6 +23,19 @@ public class KnightBossMovingState : IState {
 
 	public void ExitState()
 	{
+		
+	}
+
+	private IEnumerator Move(float speed)
+	{
+		//get a position to go to
+		Transform[] possibleLocations = controller.movementController.possibleLocations;
+		Vector3 pos = possibleLocations[Random.Range(0, possibleLocations.Length)].position;
+		//loop until get a position that is not the current position
+		while(pos == controller.transform.position)
+			pos = possibleLocations[Random.Range(0, possibleLocations.Length)].position;
+		//switch state after finish moving
+		yield return controller.movementController.MoveTo(pos, speed);
 		controller.SwitchState();
 	}
 }
