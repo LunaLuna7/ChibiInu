07d8f4bb2d40b14b947b405d053ceac817609803
Luis Luna <lfluna@uci.edu>
2019-03-07 13:03:44 -0800

    Pause fix

---
+++ ChibiInuGame/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu: MonoBehaviour{
+
+    public LevelChanger levelChanger;
+    public GameObject pauseWindow;
+    private bool loadingScene;
+
+    private void Start()
+    {
+        loadingScene = false;
+        if (levelChanger == null)
+            levelChanger = GameObject.FindObjectOfType<LevelChanger>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !loadingScene)
+        {
+            if (!pauseWindow.activeSelf)
+                ActivatePause();
+            else
+                DeactivatePause();
+        }
+    }
+
+    public void ActivatePause()
+    {
+        pauseWindow.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void DeactivatePause()
+    {
+        pauseWindow.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void LoadLevelSelect()
+    {
+        loadingScene = true;
+        levelChanger.FadeToLevel(1);
+        DeactivatePause();
+    }
+
+    public void LoadMainMenu()
+    {
+        loadingScene = true;
+        levelChanger.FadeToLevel(0);
+        DeactivatePause();
+    }
+
+}
