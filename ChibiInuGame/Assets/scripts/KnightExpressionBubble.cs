2778d41490ffac66a0c50eab5f232b424cc4ae7d
Luis Luna <lfluna@uci.edu>
2019-03-09 21:16:06 -0800

    Knight prefabs update

--- ChibiInuGame/Assets/scripts/KnightExpressionBubble.cs
+++ ChibiInuGame/Assets/scripts/KnightExpressionBubble.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class KnightExpressionBubble : MonoBehaviour {
 
     StateController stateController;
+    public GameObject spotted;
+    public GameObject lost;
+    bool playerLost;
+    bool playerFound;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        stateController = GetComponentInParent<StateController>();
+        playerLost = true;
+        playerFound = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Chasing() && playerLost)
+        {
+            //StopAllCoroutines();
+            StartCoroutine(ActivateBubble());
+            playerLost = false;
+            playerFound = true;
+        }
+        else if(!Chasing() && playerFound)
+        {
+            // StopAllCoroutines();
+            StartCoroutine(ActivateLost());
+            playerLost = true;
+            playerFound = false;
+        }
 	}
 
-    bool CheckIfChasing()
+    bool Chasing()
     {
-        return stateController.currentState.sceneGizmoColor == Color.red;
+        return stateController.playerInRange && !stateController.player.GetComponent<CharacterController2D>().m_OnShield;
+    }
+
+    IEnumerator ActivateBubble()
+    {
+        spotted.SetActive(true);
+        SoundEffectManager.instance.Play("exclamation_expression");
+        yield return new WaitForSeconds(1f);
+        spotted.SetActive(false);
+    }
+
+    IEnumerator ActivateLost()
+    {
+        lost.SetActive(true);
+        SoundEffectManager.instance.Play("question_expression");
+        yield return new WaitForSeconds(1f);
+        lost.SetActive(false);
     }
 }
