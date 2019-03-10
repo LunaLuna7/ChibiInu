46e45b69487c4d58e340c106e5a108f88f2624b9
Weiyan Zhu <weiyanz@uci.edu>
2019-03-09 21:41:00 -0800

    copy useful script for Satan Boss

---
+++ ChibiInuGame/Assets/scripts/BossSatan/SatanBossStartPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatanBossStartPoint : MonoBehaviour {
+	public CharacterController2D playerController;
+	public SatanBossManager satanBoss;
+
+	private bool hasStarted = false;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		//when touching player, showing the Boss
+		if(other.tag == "Player" && !hasStarted)
+		{
+			hasStarted = true;
+			playerController.m_Paralyzed = true;
+			StartCoroutine(BattleStart());
+		}
+	}
+
+	private IEnumerator BattleStart()
+	{
+		//player and knight boss staring at each other with love for a second
+		yield return new WaitForSeconds(1);
+		//then they start to kill each other
+		playerController.m_Paralyzed = false;
+		satanBoss.StartBattle();
+	}
+
+	public void Reset()
+	{
+		hasStarted = false;
+	}
+}
