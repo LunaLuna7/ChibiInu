97c68ce010e55ae89990ea09b518a2e4590f564c
Weiyan Zhu <weiyanz@uci.edu>
2019-03-11 23:00:03 -0700

    Satan Cage

--- ChibiInuGame/Assets/scripts/BossSatan/SatanBossPhaseManager.cs
+++ ChibiInuGame/Assets/scripts/BossSatan/SatanBossPhaseManager.cs
@@ -6,6 +6,8 @@ public class SatanBossPhaseManager : MonoBehaviour {
 	int phase = 0;
 	//maps
 	public GameObject[] maps;
+	public Transform[] playerStartPositions;
+	public GameObject player;
 	private GameObject currentMap;
 
 
@@ -17,14 +19,23 @@ public class SatanBossPhaseManager : MonoBehaviour {
 
 	private void GoToPhase(int index)
 	{
+		//hide last map
+		if(currentMap)
+			currentMap.SetActive(false);
+		//set current map
 		currentMap = maps[index];
+		currentMap.SetActive(true);
+		player.transform.position = playerStartPositions[index].position;
 	}
 
 	public void Reset()
 	{
 		phase = 0;
-		//GoToPhase(phase);
-
+		currentMap = null;
+		//hide maps for phase 1-3
+		for(int x = 1; x < maps.Length; ++x)
+			maps[x].SetActive(false);
+		GoToPhase(phase);
 	}
 
 }
