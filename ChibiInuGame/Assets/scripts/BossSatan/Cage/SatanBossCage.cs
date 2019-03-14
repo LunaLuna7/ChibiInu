2ba12dded7b4c4d0eb3e2e1cc2578cee6a41049c
Weiyan Zhu <weiyanz@uci.edu>
2019-03-14 15:59:32 -0700

    Satan Cage movement

--- ChibiInuGame/Assets/scripts/BossSatan/Cage/SatanBossCage.cs
+++ ChibiInuGame/Assets/scripts/BossSatan/Cage/SatanBossCage.cs
@@ -6,20 +6,50 @@ public class SatanBossCage : MonoBehaviour {
 	[SerializeField]private int health;
 	public int maxHealth;
 	[System.NonSerialized]public float timeTrack;
-	public float timeBeforeDamageAgain;
-
 	private bool broken = false;
 	public SatanBossPhaseManager satanBossPhaseManager;
+	public float timeBeforeDamageAgain;
+	//movement 
+	public bool moving;
+	public float moveSpeed;
+	public Transform[] positionList;
+	private int currentPositionIndex;
+	
 
 	void OnEnable()
 	{
 		Reset();
+	}
+
+	private float ratio = 0;
+	private float totalLength;
+	public void Update()
+	{
+		int next = (currentPositionIndex + 1)% positionList.Length;
+		transform.position = Vector3.Lerp(positionList[currentPositionIndex].position, positionList[next].position, ratio);
+		//change ratio
+		ratio += (moveSpeed * Time.deltaTime)/totalLength;
+		if(ratio > 1)
+		{
+			ratio = 0;
+			currentPositionIndex = next;
+			next = (currentPositionIndex + 1)% positionList.Length;
+			totalLength = Vector2.Distance(positionList[currentPositionIndex].position, positionList[next].position);
+		}
 	}
 	
 	public void Reset()
 	{
 		health = maxHealth;
 		broken = false;
+		currentPositionIndex = 0;
+		ratio = 0;
+		if(moving && positionList.Length > 0)
+		{
+			transform.position = positionList[0].position;
+			int next = (currentPositionIndex + 1)% positionList.Length;
+			totalLength = Vector2.Distance(positionList[currentPositionIndex].position, positionList[next].position);
+		}
 	}
 
 	public void TakeDamage(int amount)
@@ -32,21 +62,6 @@ public class SatanBossCage : MonoBehaviour {
 		}
 	}
 
-/* 
-	IEnumerator BlinkSprite()
-    {
-        for (int i = 0; i < 6; ++i)
-        {
-            yield return new WaitForSeconds(.05f);
-            if (m_SpriteRender.enabled == true)
-            {
-                m_SpriteRender.enabled = false;  //make changes
-            }
-            else
-            {
-                m_SpriteRender.enabled = true;   //make changes
-            }
-        }
-    }
-*/
+
+
 }
