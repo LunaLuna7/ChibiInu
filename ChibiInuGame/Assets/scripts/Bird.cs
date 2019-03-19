e6a1665eda3736a89591a3e6ee7224fc0340cc1a
Luis Luna <lfluna@uci.edu>
2019-03-19 15:27:05 -0700

    level luna update and ch1lv2 shootrate update

---
+++ ChibiInuGame/Assets/scripts/Bird.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public bool triggerFly;
+    public float speed;
+    public float timeBeforeFlyAway;
+    public Transform playerTransform;
+    public float xDistanceMax;
+    public float xDistanceMin;
+    public float yDistance;
+    
+    // Use this for initialization
+	void Start () {
+        triggerFly = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (triggerFly)
+            FlyAway();
+	}
+
+    private void FlyAway()
+    {
+        float xDistance = Random.Range(xDistanceMin, xDistanceMax);
+        if (playerTransform.position.x - transform.position.x < 0)//coming from Right
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                new Vector3(transform.position.x + xDistance, transform.position.y + yDistance, 0), speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                new Vector3(transform.position.x - xDistance, transform.position.y + yDistance, 0), speed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ ChibiInuGame/Assets/scripts/BirdManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdManager : MonoBehaviour {
+
+    public List<Bird> birds;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < this.transform.childCount; i++)
+        {
+            birds.Add(transform.GetChild(i).GetComponent<Bird>());
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            for(int i = 0; i < birds.Count; i++)
+            {
+                birds[i].triggerFly = true;
+            }
+        }
+    }
+}
