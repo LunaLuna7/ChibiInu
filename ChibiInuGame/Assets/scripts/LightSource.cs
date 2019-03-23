b6035ac45ec74ee36c869814b72d37f4e380576d
Luis Luna <lfluna@uci.edu>
2019-03-23 02:33:26 -0700

    Ch3lv1 update
    -Improve location of coins
    -added secret passages
    -included lightmap and shader grid to have light

---
+++ ChibiInuGame/Assets/scripts/LightSource.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSource : MonoBehaviour {
+
+    public float lightTimeChange;
+    public float offsetRange;
+    Light light;
+    private float originalRange;
+
+	void Awake () {
+        light = GetComponent<Light>();
+        originalRange = light.range;
+    }
+
+	// Use this for initialization
+    void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(PulseLight());    
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    IEnumerator PulseLight()
+    {
+        while (true)
+        {
+            light.range = originalRange + offsetRange;
+            yield return new WaitForSeconds(lightTimeChange);
+            light.range = originalRange;
+            yield return new WaitForSeconds(lightTimeChange);
+        }
+    }
+}
