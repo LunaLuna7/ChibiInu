b477bb744209fa81a46d7189df48273de243262f
Luis Luna <lfluna@uci.edu>
2019-03-24 15:56:09 -0700

    Ch3lv1
    -Added lighting
    -Changed poinless section with walls for fun section with laser rush
    -Added some enemies with shield spieks

---
+++ ChibiInuGame/Assets/scripts/RushLasers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RushLasers : MonoBehaviour {
+
+    public float tempo;
+    public List<GameObject> wave;
+	
+    
+    // Use this for initialization
+	void Start () {
+        StartCoroutine(Alternate());
+	}
+	
+	IEnumerator Alternate()
+    {
+        while (true)
+        {
+            for(int i = 0; i < wave.Count; i++)
+            {
+                wave[i].SetActive(true);
+                yield return new WaitForSeconds(tempo);
+                wave[i].SetActive(false);
+            }
+        }
+    }
+}
