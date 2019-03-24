c3334e82c45dd242d60da99bb3d8229022f31ff4
Weiyan Zhu <weiyanz@uci.edu>
2019-03-24 13:25:33 -0700

    pixel perfect camera adjust depends on the resolution

---
+++ ChibiInuGame/Assets/scripts/Camera/CameraPixelConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+
+//change the configuration of the pixel perfect camera
+[RequireComponent(typeof(PixelPerfectCamera))]
+public class CameraPixelConfiguration : MonoBehaviour {
+	private PixelPerfectCamera ppCamera;
+	public Vector2 basedResolution = new Vector2(1920, 1080);
+	public int basedAPPU = 27;
+	private Vector2 currentResolution;
+
+	void Awake () {
+		ppCamera = GetComponent<PixelPerfectCamera>();
+	}
+
+	void Start()
+	{
+		currentResolution = new Vector2(Screen.width, Screen.height);
+		ModifyConfiguration(currentResolution);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentResolution != new Vector2(Screen.width, Screen.height))
+		{
+			currentResolution = new Vector2(Screen.width, Screen.height);
+			ModifyConfiguration(currentResolution);
+		}
+	}
+
+	public void ModifyConfiguration(Vector2 resolution)
+	{
+		ppCamera.refResolutionX = (int)resolution.x;
+		ppCamera.refResolutionY = (int)resolution.y;
+		//calculate the new APPU
+		float ratioW = resolution.x / basedResolution.x;
+		float ratioH = resolution.y / basedResolution.y;
+		int newAPPU = (int)(basedAPPU * (ratioH + ratioW)/2 + 0.5);
+		ppCamera.assetsPPU = newAPPU;
+	}
+
+}
